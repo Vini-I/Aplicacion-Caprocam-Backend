5befa29c162919aa043927c7e20beccfe545d17b
Ninjasuperestudioso <jocamposra@est.utn.ac.cr>
2026-06-29 23:33:04 -0600

    Se agrega el respuestaJson.js y se actualizan los import de la app.js por el cambio en el packaje.json

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
+import express from "express";
+import rutaProveedor from "./routes/proveedor.route.js";
+
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-})
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.use("/api/v1/proveedores", rutaProveedor);
+
 
 app.listen(PORT, () => {
-    console.log(`El server esta corriendo en http://localhost:${PORT}`)
-})
+    console.log(`El server esta corriendo en http://localhost:${PORT}`);
+});
