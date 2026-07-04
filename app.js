29f1a5d16b0cfbfc19d4f0e71b3c4a38957a949e
Brandon-Valdelomar <brvaldelomarco@est.utn.ac.cr>
2026-07-04 15:10:34 -0600

    Controladores, Routes y Middlewares de Trazabilidad y FisicoQuimicos
    Se integran los controladores, routes y middlewares de los modulos de trazabilidad y fisicoQuimicos siguiendo el estandar indicado.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const express = require('express');
+import express from 'express';
+import fisicoQuimicaRoutes from './routes/fisicoQuimica.routes.js';
+import trazabilidadRoutes from './routes/trazabilidad.routes.js';
+
 const app = express();
 const PORT = 4000;
 
@@ -6,8 +9,11 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
-})
+});
+
+app.use('/api/v1/lecturasFisicoQuimicas', fisicoQuimicaRoutes);
+app.use('/api/v1/registrosTrazabilidad', trazabilidadRoutes);
 
 app.listen(PORT, () => {
-    console.log(`El server esta corriendo en http://localhost:${PORT}`)
-})
+    console.log(`El server esta corriendo en http://localhost:${PORT}`);
+});
